af26637fddf1b9a2a34db1205af7bc6d966a4892
Sebastian Sierra <sebastian200902@hotmail.com>
2022-08-22 18:22:17 -0300

    implementando js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,33 @@
+const entrada1 = document.getElementById("entrada1");
+const btn = document.getElementById("guardar");
+const pResult = document.getElementById("textoFinal");
+
 function editarTexto() {    
-    document.getElementById("edit-acercade").style.display="block";
-    const texto = document.getElementById("text-acercade").innerHTML;
-    console.log(texto);
+    document.getElementById("entrada1").style.display="block";
+    document.getElementById("guardar").style.display="block";
        
 }
+function btnOnClick(){
+
+    pResult.innerText = entrada1.value;
+    document.getElementById("guardar").style.display="none";
+    document.getElementById("entrada1").style.display="none";
+}
+/*
+const h1=document.querySelector('h1');
+const input1=document.querySelector('#calculo1');
+const input2=document.querySelector('#calculo2');
+const btn=document.querySelector('#btnCalcular');
+const pResult=document.querySelector('#result');
+function btnOnClick(){
+    //const sumaInputs=input1.value+input2.value;
+    //pResult.innerText="Resultado: "+sumaInputs;
+    pResult.innerText=input1.value;
+}
+
+
 function myFunction(valor){
-    document.getElementById("text-acercade").innerHTML = valor;
+    document.getElementById("textoFinal").innerHTML = valor;
 }
 function logMessage(message){
     console.log(message + "<br>");
@@ -17,3 +39,4 @@ textarea.addEventListener('keyup',(e) =>{
         document.getElementById("edit-acercade").style.display="none"
     }
 });
+*/
